7df3485b1194cf8d9aec72b99983f61c616f626c
mailofnsh <mailofnsh@gmail.com>
2016-07-14 23:09:48 +0900

    by 13

--- src/item13.cpp
+++ src/item13.cpp
@@ -6,31 +6,60 @@
  */
 #include <iostream>
 #include <memory>
+#include <tr1/memory>
+
 using namespace std;
 
-class Investment {
+class Investment
+{
 public:
-	Investment() {
+	Investment()
+	{
 		cout << "Investment()" << endl;
 	}
-	~Investment() {
+	~Investment()
+	{
 		cout << "~Investment()" << endl;
 	}
 };
 
-Investment* createInvestment() {
+Investment* createInvestment()
+{
 	return new Investment;
 }
 
-void f() {
+void ferror()
+{
+	auto_ptr<string> aps(new string[10]);
+	std::tr1::shared_ptr<int> spi(new int[1024]);
+}
+
+#if 0
+void f()
+{
 	cout << "start void f()" << endl;
-	auto_ptr<Investment> pInv(createInvestment());
+	auto_ptr<Investment> pInv1(createInvestment());
+	auto_ptr<Investment> pInv2(pInv1); // pInv1 == null;
+	pInv1 = pInv2;// pInv2 == null
+	cout << "end void f()" << endl;
+}
+#else
+void f()
+{
+	cout << "start void f()" << endl;
+
+	std::tr1::shared_ptr<Investment> pInv1(createInvestment());
+	std::tr1::shared_ptr<Investment> pInv2(pInv1);
+	pInv1 = pInv2;
 
 	cout << "end void f()" << endl;
 }
 
+#endif
+
 #if 0
-void f() {
+void f()
+{
 	cout << "start void f()" << endl;
 	Investment* pInv = createInvestment();
 
@@ -40,7 +69,9 @@ void f() {
 }
 #endif
 
-int main() {
-	f();
+int main()
+{
+	//f();
+	ferror();
 }
 
