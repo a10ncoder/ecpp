717a19b796c8a72df53ecb1b4cc835f1e944fe95
mailofnsh <mailofnsh@gmail.com>
2016-07-21 23:09:13 +0900

    by 34

--- src/item34.cpp
+++ src/item34.cpp
@@ -6,7 +6,7 @@
  */
 
 #include <iostream>
-
+#if 0
 class GameCharacter {
 public:
 	int healthValue() const {
@@ -34,6 +34,7 @@ private:
 		return 200;
 	}
 };
+#endif
 
 #if 0
 class GameCharacter {
@@ -43,9 +44,165 @@ public:
 
 #endif
 
+class Airport {
+
+};
+
+class Airplane {
+public:
+	virtual void fly(const Airport& dest) = 0;
+
+	virtual ~Airplane() {
+	}
+};
+
+void Airplane::fly(const Airport& dest) {
+	std::cout << "Airplane.fly()" << std::endl;
+}
+
+class ModelA: public Airplane {
+public:
+	virtual void fly(const Airport& dest) {
+		Airplane::fly(dest);
+	}
+};
+
+class ModelB: public Airplane {
+public:
+	virtual void fly(const Airport& dest) {
+		Airplane::fly(dest);
+	}
+};
+
+class ModelC: public Airplane {
+public:
+	virtual void fly(const Airport& dest) {
+		std::cout << "ModelC.fly()" << std::endl;
+	}
+};
+
+#if 0
+class Airport {
+
+};
+
+class Airplane {
+public:
+	virtual void fly(const Airport& dest) = 0;
+
+	virtual ~Airplane() {
+	}
+protected:
+	void defaultFly(const Airport& dest) {
+		std::cout << "Airplane.fly()" << std::endl;
+	}
+};
+
+class ModelA: public Airplane {
+public:
+	virtual void fly(const Airport& dest) {
+		Airplane::defaultFly(dest);
+	}
+};
+
+class ModelB: public Airplane {
+public:
+	virtual void fly(const Airport& dest) {
+		Airplane::defaultFly(dest);
+	}
+};
+
+class ModelC: public Airplane {
+public:
+	virtual void fly(const Airport& dest) {
+		std::cout << "ModelC.fly()" << std::endl;
+	}
+};
+
+#endif
+
+#if 0
+class Airport {
+
+};
+
+class Airplane {
+public:
+	virtual void fly(const Airport& dest) {
+		std::cout << "Airplane.fly()" << std::endl;
+	}
+
+	virtual ~Airplane() {
+	}
+};
+
+class ModelA:public Airplane {
+
+};
+
+class ModelB:public Airplane {
+
+};
+
+class ModelC:public Airplane {
+
+};
+
+#endif
+
+#if 0
+class Shape {
+public:
+	virtual void draw() const = 0;
+	virtual void error(const std::string& msg) {
+	}
+	int objectId() const {
+		return 0;
+	}
+
+	virtual ~Shape() {
+	}
+};
+
+class Rectangle: public Shape {
+public:
+	virtual void draw() const {
+		std::cout << "Rectangle.draw()" << std::endl;
+	}
+};
+class Ellipse: public Shape {
+public:
+	virtual void draw() const {
+		std::cout << "Ellipse.draw()" << std::endl;
+	}
+};
+
+#endif
+
 int main() {
+
+#if 0
 	Poketmon p;
 
 	std::cout << p.healthValue() << std::endl;
+#endif
+
+#if 1
+	Airport pdx;
+	Airplane *pa = new ModelC;
+	pa->fly(pdx);
+#endif
+
+#if 0
+// Shape *ps = new Shape;// error
+	Shape *ps1 = new Rectangle;
+	ps1->draw();
+	Shape *ps2 = new Ellipse;
+	ps2->draw();
+//ps1->Shape::draw();//undefined reference
+//ps2->Shape::draw();// undefined reference
+
+#endif
+
 }
 
---
+++ src/item35.cpp
@@ -0,0 +1,10 @@
+/*
+ * item35.cpp
+ *
+ *  Created on: 2016. 7. 21.
+ *      Author: mailo
+ */
+
+
+
+
